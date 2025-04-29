cf2fe4ad12d12d1e0c7cfe38b7adf20e4088f468
zmling22 <zmling22@gmail.com>
2025-04-29 15:51:02 +0800

    update question generation

--- data/ToolTrajectory/question_generation/scripts/generate_status.sh
+++ data/ToolTrajectory/question_generation/scripts/generate_status.sh
@@ -1,5 +1,5 @@
 python data/ToolTrajectory/question_generation/generate_question_with_images.py \
 -prompt data/ToolTrajectory/prompts/status/prompt_status.txt \
 -type status \
--root data/HM3D/ 
+-root data/HM3D/ \
 -output data/ToolTrajectory/questions/status/status_question.csv
